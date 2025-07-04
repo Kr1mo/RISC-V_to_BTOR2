e81b1efe68a4ebedefcfaaba83baaaf69b00baa8
Krister <kristermoeller@gmx.net>
2025-07-04 09:06:43 +0200

    de-destroying git

--- src/state_fuzzer.c
+++ src/state_fuzzer.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <unistd.h>
 
+//test
+
 /* Generate a random generated riscv .state file.
  * This file can then be used to test the
  * the simulation via btormc against risc_v_sim.
