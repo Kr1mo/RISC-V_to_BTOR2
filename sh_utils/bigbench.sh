74b8b7d0ff862a5268c2720f0f1cfb567fe558f6
Krister <kristermoeller@gmx.net>
2025-09-08 12:32:45 +0200

    15 benchmarks are too much, 5 suffice for an average

--- sh_utils/bigbench.sh
+++ sh_utils/bigbench.sh
@@ -5,16 +5,6 @@ sh_utils/benchmark_all.sh bigbench02
 sh_utils/benchmark_all.sh bigbench03
 sh_utils/benchmark_all.sh bigbench04
 sh_utils/benchmark_all.sh bigbench05
-sh_utils/benchmark_all.sh bigbench06
-sh_utils/benchmark_all.sh bigbench07
-sh_utils/benchmark_all.sh bigbench08
-sh_utils/benchmark_all.sh bigbench09
-sh_utils/benchmark_all.sh bigbench10
-sh_utils/benchmark_all.sh bigbench11
-sh_utils/benchmark_all.sh bigbench12
-sh_utils/benchmark_all.sh bigbench13
-sh_utils/benchmark_all.sh bigbench14
-sh_utils/benchmark_all.sh bigbench15
 
 for file in benchmark_files/bench*.log; do
     TYPE=$(basename "$file" | cut -d'_' -f2 | cut -d'.' -f1)
